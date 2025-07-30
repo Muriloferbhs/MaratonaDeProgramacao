75be543c9f3e9609eafb57ad461af45271dda1e8
Muriloferbhs <muriloferbhs@gmail.com>
2025-07-30 15:41:12 -0300

    liga autosave

---
+++ Estoque.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Estoque {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Estoque de produtos");
+
+        sc.close();
+
+    }
+}

3df16de16d778330af6ed9f4749f6ff68e3a3b9f
Muriloferbhs <muriloferbhs@gmail.com>
2025-07-30 16:10:17 -0300

    Termina o VAR.java

--- VAR.java
+++ VAR.java
@@ -4,7 +4,15 @@ public class VAR {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        System.out.println("maconha");
+        int x = sc.nextInt();
+        int y = sc.nextInt();
+
+        if(8 >= x && x >= -8 && 8 >= y && y >= 0) {
+            System.out.println("S");
+        } else {
+            System.out.println("N");
+        }
+
 
         sc.close();
     }
